27055f2339fd5ff3f1ce3f13895836084a0db69b
Kimleng <141606539+kimlengthai@users.noreply.github.com>
2025-08-18 19:45:38 +1000

    Add X in the middle of the transition scene
    I will also do it for the QuestionScene and ArticleScene.

--- Assets/Script/SceneTransitionManager.cs
+++ Assets/Script/SceneTransitionManager.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro; // needed for TextMeshPro
 
 public class SceneTransitionManager : MonoBehaviour
 {
     public float transitionDelay = 1.0f;
+    public TextMeshProUGUI transitionText;
 
     private void Start()
     {
@@ -18,6 +20,15 @@ public class SceneTransitionManager : MonoBehaviour
         Debug.Log($"[TransitionScene] Waiting {transitionDelay} seconds to load '{nextScene}'...");
 
         Invoke("LoadNextScene", transitionDelay);
+    }
+
+    private void Update()
+    {
+        // Keep updating the "X" while in transition
+        if (transitionText != null)
+        {
+            transitionText.text = "X";
+        }
     }
 
     void LoadNextScene()
